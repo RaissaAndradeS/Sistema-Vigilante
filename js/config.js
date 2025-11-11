03a9eca3240c2452b105a9433a80448fc16e3812
RaissaAndradeS <221035077@aluno.unb.br>
2025-11-11 15:33:07 -0300

    Restore config.js with ADMIN_PASSWORD for login functionality

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const ADMIN_PASSWORD = '197609';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjukYZ483MuTh77jRsO-PmzCRw_RNebYM",
+  authDomain: "sistema-vigilantes.firebaseapp.com",
+  projectId: "sistema-vigilantes",
+  storageBucket: "sistema-vigilantes.firebasestorage.app",
+  messagingSenderId: "721178326261",
+  appId: "1:721178326261:web:cfdd249d3e8bd8f39601bc"
+};
